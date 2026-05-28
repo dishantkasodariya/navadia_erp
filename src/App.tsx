44660cafdcb1faef218f84423573a2c5666d96ea
Dishant200301 <dishantkasodariya2003@gmail.com>
2026-05-28 14:15:45 +0530

    erp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import React from "react";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
-import "@fontsource/dm-serif-display/400.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
